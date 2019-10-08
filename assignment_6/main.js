7f7b2c6fcdcf7b4721ebdcb99dc179281d35bf84
Lana Takemoto <lanaboo@takemotananoair.wifi.smc.edu>
2019-10-08 14:42:46 -0700

    Populate state dropdown
    Added Javascript to populate state input depending on what you chose on the previous dropdown input

--- assignment_6/main.js
+++ assignment_6/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $("#sameAddress").on("change", function(){
+$(document).ready(function () {
+    $("#sameAddress").on("change", function () {
         if (this.checked) {
             $("[name='shipping_first_name']").val($("[name='first_name']").val());
             $("[name='shipping_last_name']").val($("[name='last_name']").val());
@@ -9,10 +9,10 @@ $(document).ready(function(){
             $("[name='shipping_city']").val($("[name='billing_city']").val());
             $("[name='shipping_state']").val($("[name='billing_state']").val());
             $("[name='shipping_zip']").val($("[name='billing_zip']").val());
-          }
-    });// end on
+        }
+    }); // end on
 
-    $("#differentAddress").on("change", function(){
+    $("#differentAddress").on("change", function () {
         if (this.checked) {
             $("[name='shipping_first_name']").val('');
             $("[name='shipping_last_name']").val('');
@@ -22,14 +22,35 @@ $(document).ready(function(){
             $("[name='shipping_city']").val('');
             $("[name='shipping_state']").val('');
             $("[name='shipping_zip']").val('');
-          }
-    });// end on
+        }
+    }); // end on
 
-    
 
-    $("#city").change(function(){
-        $("#state").val() === this.val();
-    });
-    
+    var cityDropdown = document.getElementById('city');
+    var stateDropdown = document.getElementById('state');
+
+    cityDropdown.addEventListener('change', function () {
+        if (this.value === '1')
+            stateDropdown.innerHTML = '<option value="' + stateDropdown[0] + '">' + "California" + '</option>';
+        if (this.value === '2')
+            stateDropdown.innerHTML = '<option value="' + stateDropdown[1] + '">' + "New York" + '</option>';
+        if (this.value === '3')
+            stateDropdown.innerHTML = '<option value="' + stateDropdown[2] + '">' + "Arizona" + '</option>';
+    }); // end billing state dropdown
+
+    var cityDropdown2 = document.getElementById('shipping_city');
+    var stateDropdown2 = document.getElementById('shipping_state');
+
+    cityDropdown2.addEventListener('change', function () {
+        if (this.value === '1')
+            stateDropdown2.innerHTML = '<option value="' + stateDropdown2[0] + '">' + "California" + '</option>';
+        if (this.value === '2')
+            stateDropdown2.innerHTML = '<option value="' + stateDropdown2[1] + '">' + "New York" + '</option>';
+        if (this.value === '3')
+            stateDropdown2.innerHTML = '<option value="' + stateDropdown2[2] + '">' + "Arizona" + '</option>';
+    }); // end shipping state dropdown
+
+
+
 
 }); // end ready
